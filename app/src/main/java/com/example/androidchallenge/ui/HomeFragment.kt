d70c3221c25d1053c924860e4a017cc1c3deb998
Brian <briangalarza@hotmail.com>
2022-12-15 09:09:09 -0300

    Files location changed

--- app/src/main/java/com/example/androidchallenge/ui/HomeFragment.kt
+++ app/src/main/java/com/example/androidchallenge/ui/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.androidchallenge
+package com.example.androidchallenge.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.androidchallenge.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
--- app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.example.androidchallenge
+package com.example.androidchallenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidchallenge.R
+import org.koin.core.context.startKoin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startKoin {
+
+        }
 
     }
 }
