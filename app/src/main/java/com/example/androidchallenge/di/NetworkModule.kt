42d6ad6242647ee4c2f48076ca3d3049ebbd785b
Brian <briangalarza@hotmail.com>
2022-12-16 06:43:23 -0300

    networkModule created

---
+++ app/src/main/java/com/example/androidchallenge/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package com.example.androidchallenge.di
+
+import com.example.androidchallenge.BuildConfig
+import com.example.androidchallenge.repository.api.PhotosApi
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+    factory { provideOkHttpClient() }
+    factory { providePhotosApi(get()) }
+    single { provideRetrofit(get()) }
+}
+
+
+fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+    Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create()).client(okHttpClient).build()
+
+fun providePhotosApi(retrofit: Retrofit): PhotosApi = retrofit.create(PhotosApi::class.java)
+
+
