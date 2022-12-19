e0fdab762445492bcffd9763c597ca0f35ad7b62
Brian <briangalarza@hotmail.com>
2022-12-19 07:35:54 -0300

    Toolbar added

--- app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.example.androidchallenge.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.androidchallenge.R
-import org.koin.core.context.startKoin
+import com.example.androidchallenge.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        startKoin {
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.navigation_host
+        ) as NavHostFragment
+        navController = navHostFragment.navController
 
+        appBarConfiguration= AppBarConfiguration(navController.graph)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navigation_host).navigateUp(
+            appBarConfiguration
+        )
     }
 }
